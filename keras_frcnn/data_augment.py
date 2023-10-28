092a7491dc748577c7acf74cccada0888321c826
a-gentili <55603257+a-gentili@users.noreply.github.com>
2023-10-28 11:52:55 +0200

    Update data_augment.py

--- keras_frcnn/data_augment.py
+++ keras_frcnn/data_augment.py
@@ -19,7 +19,8 @@ def augment(img_data, config, augment=True):
 
 		if config.channel_shift and np.random.randint(0, 2) == 0:
 			order = random.sample(range(3),3)
-			img = cv2.merge((img[:,:,order[0]],img[:,:,order[1]],img[:,:,order[2]]))
+			img_aux = copy.deepcopy(img)
+			img = cv2.merge((img_aux[:,:,order[0]],img_aux[:,:,order[1]],img_aux[:,:,order[2]]))
 			
 		if config.use_horizontal_flips and np.random.randint(0, 2) == 0:
 			img = cv2.flip(img, 1)
